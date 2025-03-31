c064c7494d71bf8e6fc36fad968057a700dc388f
Polisetty Ramya Sri <polisettyramyasri12@gmail.com>
2025-03-31 17:40:59 +0530

    practice

---
+++ problems/personal.py
@@ -0,0 +1,4 @@
+# Write a Python program that displays your name, age, and address on three different lines.
+name,age="Simon",19
+address="Bangalore, Karnataka, India"
+print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
